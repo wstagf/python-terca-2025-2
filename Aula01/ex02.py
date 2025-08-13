deb6bb3cb13367b51deb00e8defdcce45dc8131a
Thiago Augusto <wstagf@gmail.com>
2025-08-12 21:02:32 -0300

    Execicio 02 e 03 de algoritmos em python

---
+++ Aula01/ex02.py
@@ -0,0 +1,12 @@
+# Faça um Programa que peça um número
+#  e então mostre a mensagem: 
+# O número informado foi [número]. 
+
+numero = input("Digite um valor: ")
+
+print("O numero digitado é: " + numero)
+
+
+# concatenar é juntar TEXTOS
+# nome + somemenonme // nome completo
+# rua + numero + cidade + qd // endereco completo
---
+++ Aula01/ex03.py
@@ -0,0 +1,11 @@
+# Faça um Programa que peça dois números e imprima a soma.
+
+
+numero1 = int(input("Digite um valor1: "))
+numero2 = int(input("Digite um valor2: "))
+ 
+
+soma = numero1 + numero2
+
+print("A soma de "+ str(numero1) +" e " + str(numero2) +" é: "+ str(soma)+".")
+ 
