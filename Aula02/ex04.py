08dc0fdf2802d28a42192ec3048fb39c054f96d4
Thiago Augusto <wstagf@gmail.com>
2025-08-26 19:46:15 -0300

    executado primeiro exercicio

---
+++ Aula02/ex04.py
@@ -0,0 +1,14 @@
+# 4. Faça um Programa que peça as 
+# 4 notas bimestrais e mostre a média.
+
+
+n1 = float(input("Digite uma nota1: "))
+n2 = float(input("Digite uma nota2: "))
+n3 = float(input("Digite uma nota3: "))
+n4 = float(input("Digite uma nota4: "))
+
+media = (n1+n2+n3+n4)/4
+
+print("A media das notas é ")
+print(media)
+
